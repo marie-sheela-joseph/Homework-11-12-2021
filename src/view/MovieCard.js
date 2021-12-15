d10f2bae5438eda327eee3a80b1973db3728d251
Kailas Walldoddi <kailas.walldoddi@ellucian.com>
2021-12-15 14:17:58 +0530

    removed like/dislike/delete buttons

--- src/view/MovieCard.js
+++ src/view/MovieCard.js
@@ -1,37 +1,56 @@
-import React, { useState } from 'react'
-import './MovieCard.css'
+import React, { useState } from "react";
+import "./MovieCard.css";
 
-export default function MovieCard({ movie, deleteFn, toggleLike, toggleDisLike }) {
+export default function MovieCard({
+  movie,
+  deleteFn,
+  toggleLike,
+  toggleDisLike,
+}) {
+  const [isLiked, setIsLiked] = useState(false);
+  const [isdisLiked, setIsDisLiked] = useState(false);
 
-    const [isLiked, setIsLiked] = useState(false)
-    const [isdisLiked, setIsDisLiked] = useState(false)
-
-    return (
-        <div className='movieCard'>
-            <h1 className='mb-10'>{movie.title}</h1>
-            <h3 className='mb-10'>{movie.category}</h3>
-            <div className='mb-10 d-flex justify-around'>
-                <span><i class="fas fa-thumbs-up"></i> {movie.likes}</span>
-                <span><i class="fas fa-thumbs-down"></i>  {movie.dislikes}</span>
-            </div>
-            <div className='mb-10 d-flex justify-center'>
-                <button onClick={() => {
-                    toggleLike(movie.id, !isLiked)
-                    setIsLiked(!isLiked)
-                }}
-                    className='p-5 mh'>
-                    {'Like'}
-                </button>
-                <button onClick={() => {
-                    toggleDisLike(movie.id, !isdisLiked)
-                    setIsDisLiked(!isdisLiked)
-                }}
-                    className='p-5 mh'>
-                    { 'Dislike'}
-                </button>
-                <button onClick={() => { deleteFn(movie.id) }}
-                    className='p-5'>Delete</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="movieCard">
+      <div style={{ textAlign: "right" }}>
+        <span
+          onClick={() => {
+            deleteFn(movie.id);
+          }}
+        >
+          <i class="fa fa-trash"></i>
+        </span>
+      </div>
+      <h1 className="mb-10 d-flex justify-center">{movie.title} </h1>
+      <h3 className="mb-10">{movie.category}</h3>
+      <div className="mb-10 d-flex justify-around">
+        <span
+          onClick={() => {
+            toggleLike(movie.id, !isLiked);
+            setIsLiked(!isLiked);
+          }}
+        >
+          <i class="fas fa-thumbs-up"></i>{" "}
+          {isLiked ? (
+            <span style={{ color: "blue" }}>{movie.likes} </span>
+          ) : (
+            movie.likes
+          )}
+        </span>
+        <span
+          onClick={() => {
+            toggleDisLike(movie.id, !isdisLiked);
+            setIsDisLiked(!isdisLiked);
+          }}
+        >
+          <i class="fas fa-thumbs-down"></i>{" "}
+          {isdisLiked ? (
+            <span style={{ color: "red" }}>{movie.dislikes} </span>
+          ) : (
+            movie.dislikes
+          )}
+        </span>
+      </div>
+    </div>
+  );
 }
